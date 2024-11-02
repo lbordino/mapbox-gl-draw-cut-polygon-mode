6a7f3b6afcc1e853970991c22f0ce7646390b90d
Luca Bordino <lbordino73@gmail.com>
2024-11-02 20:32:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { default as drawStyles } from "./customDrawStyles.js";
 
 import { passing_draw_polygon } from "mapbox-gl-draw-passing-mode";
 import { modeName, passingModeName } from "./constants";
-
+import { default as CutBar } from "./CutBar.tsx";
 export { cutPolygonMode as splitPolygonMode };
 export { drawStyles };
-
+export { CutBar };
 export default function CutPolygonMode(modes) {
   return {
     ...modes,
